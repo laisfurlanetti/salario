d9888eed52b87819be9b5e2772a69d505d1b966d
adriano.exe <58786675+adriano@users.noreply.github.com>
2020-05-13 19:31:43 -0300

    correcao replace

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace salario
         static void Main(string[] args)
         {
             decimal salario;
-            decimal porcent = 0;
+            decimal porcentagem = 0;
             decimal num = 0;
             decimal resultado = 0;
 
@@ -21,35 +21,35 @@ namespace salario
             if (salario <= 280)
             {
                 num = 20;
-                porcent = (salario * num) / 100;
-                resultado = porcent + salario;
+                porcentagem = (salario * num) / 100;
+                resultado = porcentagem + salario;
             }
 
             if (salario >= 280 && salario < 700)
             {
                 num = 15;
-                porcent = (salario * num) / 100;
-                resultado = porcent + salario;
+                porcentagem = (salario * num) / 100;
+                resultado = porcentagem + salario;
             }
 
             if (salario >= 700 && salario < 1500)
             {
                 num = 10;
-                porcent = (salario * num) / 100;
-                resultado = porcent + salario;
+                porcentagem = (salario * num) / 100;
+                resultado = porcentagem + salario;
             }
 
             if (salario > 1500)
             {
                 num = 5;
-                porcent = (salario * num) / 100;
-                resultado = porcent + salario;
+                porcentagem = (salario * num) / 100;
+                resultado = porcentagem + salario;
             }
 
             Console.WriteLine("Seu salario era de: R$ " + salario);
             Console.WriteLine("Seu percentual foi de " + num + "%");
             Console.WriteLine("Seu salario atual e de: R$" + resultado);
-            Console.WriteLine("O aumento foi de: " + porcent);
+            Console.WriteLine("O aumento foi de: " + porcentagem);
 
             Console.ReadLine();
 
